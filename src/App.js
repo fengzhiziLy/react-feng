4ba80ca405582d6090b1645ddcdd6a1d725d8507
fengzhiziLy <Ly332883899@163.com>
2018-06-28 01:52:48 +0800

    update first

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+// {Component} === React.Component  ES6的解构赋值
+// import { Component } from 'react'    等价于
+// import React from 'react'
+// const Component = React.Component
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        hello feng
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+// all in js --- 一种理念
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+// PWA progressive web application
+// https协议的服务器上，在断网的情况下可以将网页暂存到浏览器中
+// import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// registerServiceWorker();
